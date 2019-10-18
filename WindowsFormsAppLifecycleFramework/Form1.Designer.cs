f6ee5ae7ce73d904a21a57e0969a061d5f5298c4
Joe Scheiman <scheiman.joe@gmail.com>
2019-10-18 10:00:05 -0400

    first commit

---
+++ WindowsFormsAppLifecycleFramework/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+
+namespace WindowsFormsAppLifecycleFramework
+{
+    partial class Form1
+    {
+        
+        StreamWriter sr = new StreamWriter("formevents.txt");
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            sr.Close();
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsAppLifecycleFramework/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLifecycleFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            sr.WriteLine("1 - MOVE event");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sr.WriteLine("2 - LOAD event");
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+            sr.WriteLine("3 - VisibleChanged event");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            sr.WriteLine("4 - ACTIVATED event");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            sr.WriteLine("5 - SHOWN event");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            sr.WriteLine("6 - PAINT event");
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            sr.WriteLine("7 - DEACTIVATE event");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sr.WriteLine("8 - FormClosing event");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sr.WriteLine("9 - FormCLOSED event");
+        }
+
+        
+    }
+}
